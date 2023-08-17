94eb0374ec2884227c023a100d5ab76f0e0f8368
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-08-18 03:37:43 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
 $dotenv->load();
 
 $db = new mysqli;
